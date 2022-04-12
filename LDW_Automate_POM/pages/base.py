be771bcd41e4a9116050618100dc285aa66d2ed9
jianing <sjian199087@gamil.com>
2022-04-12 18:32:01 +0800

    1.0

--- LDW_Automate_POM/pages/base.py
+++ LDW_Automate_POM/pages/base.py
@@ -15,15 +15,22 @@ import os
 from PIL import Image
 from PIL import ImageEnhance
 from pytesseract import pytesseract
+from selenium.webdriver.chrome.options import Options
 from selenium.common import exceptions
 
 class Base_pages(object):
     #初始化driver、url
     def __init__(self,driver,base_url):
+        chrome_options=Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("window-size=1920x1080")
+        chrome_options.add_argument("--disable-gpu")
+        self.driver = webdriver.Chrome(options=chrome_options)
         self.driver=driver
         self.base_url=base_url
         self.driver.implicitly_wait(5)
-        self.driver.maximize_window()
+        #self.driver.maximize_window()
     #查找元素
     def Find_element(self,elem):
         #return self.driver.find_element(*elem)
