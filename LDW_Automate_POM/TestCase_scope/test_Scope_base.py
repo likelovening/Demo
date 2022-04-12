c88cf0e830b086a5931b9341de3428fa9aee2f66
jianing <sjian199087@gamil.com>
2022-04-12 17:23:55 +0800

    1.0

--- LDW_Automate_POM/TestCase_scope/test_Scope_base.py
+++ LDW_Automate_POM/TestCase_scope/test_Scope_base.py
@@ -22,6 +22,8 @@ class Test_scope():
         chrome_options=Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("window-size=1920x1080")
+        chrome_options.add_argument("--disable-gpu")
         self.driver = webdriver.Chrome(chrome_options=chrome_options)
         # yield
         # self.driver.quit()
