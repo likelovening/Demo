1d57c3c06a05f13c2b73ed06c7fd03d3be36ccad
jianing <sjian199087@gamil.com>
2022-04-13 13:50:56 +0800

    1.0

--- LDW_Automate_POM/pages/element_Aloginpage.py
+++ LDW_Automate_POM/pages/element_Aloginpage.py
@@ -6,6 +6,9 @@ _date_=21/10/20
 _data_='封装登陆操作'
 """
 import sys
+
+from selenium.webdriver import ActionChains
+
 sys.path.append('..')
 from selenium import webdriver
 from LDW_Automate_POM.pages.base import Base_pages
@@ -86,7 +89,9 @@ class login_LDW_new(Base_pages):
         self.driver.get(self.LDW_operation_url)                         #打开url
         self.input_send_keys(self.LDW_operation_user_name,user_name)    #输入账户
         self.input_send_keys(self.LDW_operation_user_psw,user_psw)      #输入密码
-        self.js_click(self.LDW_new2_login)                                 #点击登陆
+        self.LDW_new2_login1=Base_pages.Find_element(self.LDW_new2_login)
+        ActionChains(self.driver).move_to_element(self.LDW_new2_login1).click().perform()
+        #self.js_click(self.LDW_new2_login)                                 #点击登陆
         self.sleep_wait(2)
         self.isloginsuccess(self.LDW_new2_login_success)                #判断是否登陆成功
 
